2e4e063d7667c23291239a8f18c265b9227ffc2e
Your UserName <aratinaik151997@gmail.com>
2021-11-19 21:06:07 +0530

    Added flip coin problem have caluculated percentage of Heads and Tails

---
+++ BasicCorePrograms/BasicCorePrograms/FlipCoin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    class FlipCoin
+    {
+        public double countTails = 0, countHeads = 0;
+        public double perTails = 0, perHeads = 0;
+        public void FlipCoinProblem()
+        {
+            
+            Console.WriteLine("Problem: Flip coin and print percentage of head and tails");
+            Console.WriteLine("Enter number of times you want to flip a coin");
+            int num=Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            for (int i=0;i<num;i++)
+            {
+                int check = random.Next(0, 2);
+
+                if (check < 0.5)
+                {
+                    Console.WriteLine("Tails");
+                    countTails = countTails + 1;                    
+                }
+                else
+                {
+                    Console.WriteLine("Heads");
+                    countHeads = countHeads + 1;
+                }
+            }
+            perTails = countTails / num * 100;
+            perHeads = countHeads / num * 100;
+            Console.WriteLine("Percentage of Tails is " + perTails);
+            Console.WriteLine("Percentage of Heads is "+perHeads);
+        }
+    }
+}
---
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Core Programs");
+            FlipCoin flipCoinobj = new FlipCoin();
+            flipCoinobj.FlipCoinProblem();
+            Console.ReadKey();
+        }
+    }
+}
